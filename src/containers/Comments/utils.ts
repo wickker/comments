7cac6bfc6c6b36b2773cc29ade29f81450904b24
Dian Chun Choy <choydianchun@gmail.com>
2024-08-13 00:49:39 +0800

    add search comments func

--- src/containers/Comments/utils.ts
+++ src/containers/Comments/utils.ts
@@ -50,6 +50,21 @@ export const deleteReply = (
   }))
 }
 
-export const search = (searchPhrase: string): Comment[] => {
-  
+const search = (comments: Comment[], searchPhrase: string): Comment[] => {
+  if (comments.length === 0) return []
+
+  const filtered: Comment[] = []
+
+  for (const comment of comments) {
+    if (comment.comment.includes(searchPhrase)) {
+      filtered.push(comment)
+      continue
+    }
+    const filteredReplies = search(comment.replies, searchPhrase)
+    if (filteredReplies.length > 0) {
+      filtered.push({ ...comment, replies: filteredReplies })
+    }
+  }
+
+  return filtered
 }
